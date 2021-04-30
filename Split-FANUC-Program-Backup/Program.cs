bf807d1d89d81ecf54a4df572d0c33685848ce86
A9G-Data-Droid <akauffman@cet.aero>
2021-04-30 15:46:31 -0700

    Add project files.

---
+++ Split-FANUC-Program-Backup/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Split_FANUC_Program_Backup
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                DisplayHelp();
+                return 1;
+            } 
+
+            if (!File.Exists(args[0]))
+            {
+                NotFoundError(args[0]);
+                return 2;
+            }
+
+            SplitALLPROGtxt(args[0]);
+            return 0;
+        }
+
+        private static void SplitALLPROGtxt(string fileName)
+        {
+            string pattern = "^%((.|\n|\r)*)%$";
+            string[] result = Regex.Split(File.ReadAllText(fileName), pattern,
+                                    RegexOptions.Multiline,
+                                    TimeSpan.FromMilliseconds(5000));
+            throw new NotImplementedException();
+        }
+
+        private static void NotFoundError(string fileName)
+        {
+            Console.WriteLine("File not found: " + fileName);
+        }
+
+        static void DisplayHelp()
+        {
+            Console.WriteLine("One argument required. Enter only the path of the file you would like to split.");
+        }
+    }
+}
